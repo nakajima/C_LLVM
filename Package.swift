522b1470620ad305a18ecb7bd4d2a215f1030087
Pat Nakajima <patnakajima@gmail.com>
2024-07-18 21:38:40 -0700

    ok it didn't work

--- Package.swift
+++ Package.swift
@@ -11,10 +11,6 @@ let package = Package(
 			name: "C_LLVM",
 			targets: ["C_LLVM"]
 		),
-		.library(
-			name: "Cxx_LLVM",
-			targets: ["Cxx_LLVM"]
-		),
 	],
 	targets: [
 		.systemLibrary(
@@ -24,21 +20,10 @@ let package = Package(
 				.brew(["llvm"])
 			]
 		),
-		.systemLibrary(
-			name: "Cxx_LLVM",
-			pkgConfig: "cllvm",
-			providers: [
-				.brew(["llvm"])
-			]
-		),
 		.testTarget(
 			name: "C_LLVMTests",
 			dependencies: ["C_LLVM"]
 		),
-		.testTarget(
-			name: "Cxx_LLVMTests",
-			dependencies: ["Cxx_LLVM"]
-		)
 	],
 	cxxLanguageStandard: .cxx20
 )
